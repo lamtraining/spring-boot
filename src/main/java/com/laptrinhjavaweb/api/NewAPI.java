9fe5c1a40af16b0817c8781be3a092469fc21432
unknown <lamjavadev456@gmail.com>
2019-06-05 03:17:50 +0700

    restful web service part 2

--- src/main/java/com/laptrinhjavaweb/api/NewAPI.java
+++ src/main/java/com/laptrinhjavaweb/api/NewAPI.java
@@ -1,18 +1,28 @@
 package com.laptrinhjavaweb.api;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.laptrinhjavaweb.dto.NewDTO;
 
-@Controller
+@RestController
 public class NewAPI {
 
-	@RequestMapping(value = "/new", method = RequestMethod.POST)
-	@ResponseBody
+	@PostMapping(value = "/new")
 	public NewDTO createNew(@RequestBody NewDTO model) {
 		return model;
+	}
+	
+	@PutMapping(value = "/new")
+	public NewDTO updateNew(@RequestBody NewDTO model) {
+		return model;
+	}
+	
+	@DeleteMapping(value = "/new")
+	public void deleteNew(@RequestBody long[] ids) {
+		
 	}
 }
